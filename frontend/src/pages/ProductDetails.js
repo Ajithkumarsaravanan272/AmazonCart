7f8d0a87ce687d6346d96601fbb740322ff485d3
Ajithkumarsaravanan272 <ajithkumar2722002@gmail.com>
2025-07-19 15:15:15 +0530

    Update ProductDetails.js

--- frontend/src/pages/ProductDetails.js
+++ frontend/src/pages/ProductDetails.js
@@ -8,7 +8,7 @@ export default function ProductDetails({cartItems,setCartItems}){
     const {id}=useParams();
 
      useEffect(() => {
-            fetch(process.env.REACT_APP_API_URL +"api/products/"+id)
+            fetch(process.env.REACT_APP_API_URL +"/api/products/"+id)
             .then(res => res.json())
             .then(res => setProduct(res.product))
          },[id]);
